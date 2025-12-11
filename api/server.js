76608ea0d1238272546883097e0e47daf0b8ba28
Ali Mohamed Mansor <137876199+aliMohamed2345@users.noreply.github.com>
2025-12-11 09:06:42 +0200

    changing the server.js

--- api/server.js
+++ api/server.js
@@ -55,10 +55,5 @@ globalErrorHandler(app);
 //database connection 
 connectToDB();
 
-//server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
 
 export default app;
